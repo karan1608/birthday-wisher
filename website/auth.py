31d0dffe7535fe32a51df80c8f8cfe697dc554aa
karan1608 <karankumar.d2019@vitstudent.ac.in>
2022-04-21 15:05:30 +0530

    initial commit

---
+++ website/auth.py
@@ -0,0 +1,108 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
+import smtplib
+import pandas
+import datetime as dt
+from telethon.tl.functions.contacts import ImportContactsRequest
+from telethon.tl.types import InputPeerUser, InputPeerChannel, InputPhoneContact
+from telethon import TelegramClient, sync, events
+
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    now = dt.datetime.now()
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                print(user.notes)
+                for note in user.notes:
+                    # print("1")
+                    print(note.greet)
+                    mail = note.email
+                    name = note.name
+                    bday = note.date
+                    bday_d = int(bday.split("/")[0])
+                    bday_m = int(bday.split("/")[1])
+                    # print(type(bday_m), type(now.month))
+                    if bday_m == now.month:
+                        # print("m s")
+                        if bday_d == now.day:
+                            if note.greet == 0:
+                                #---------------------------------------------------------------------------
+                                # phone = "+919840025725"
+                                # client = TelegramClient("Test", 11922065, "bd5283ce86287d62813b1ec0fef8a9ec")
+                                # client.start()
+                                # destination_user_username = '919840046483'
+                                # entity = client.get_entity(destination_user_username)
+                                # client.send_message(entity=entity, message="Hi")
+                                #---------------------------------------------------------------------------
+                                print("sending mail...")
+                                my_email = "professorsergiomarquinasalva@gmail.com"
+                                password = "qazwsxcde12"
+
+                                connection = smtplib.SMTP("smtp.gmail.com")
+                                connection.starttls()
+                                connection.login(my_email, password)
+                                connection.sendmail(from_addr=my_email, to_addrs=mail,
+                                                    msg=f"Subject:Flask\n\nHappy Birthday {name}")
+                                note.greet = 1
+                                db.session.commit()
+                                # print(note.greet)
+                return redirect(url_for('views.home'))
+
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
+
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
+                password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
+
+    return render_template("sign_up.html", user=current_user)
---
+++ website/views.py
@@ -0,0 +1,91 @@
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
+import re
+from telethon.tl.functions.contacts import ImportContactsRequest
+from telethon.tl.types import InputPeerUser, InputPeerChannel, InputPhoneContact
+from telethon import TelegramClient, sync, events
+
+import random
+import smtplib
+import pandas
+import datetime as dt
+
+now = dt.datetime.now()
+
+views = Blueprint('views', __name__)
+
+
+def date_checker(date_date):
+    r = re.compile('.*/.*/.*:.*')
+    if r.match(date_date) is not None:
+        return True
+    else:
+        return False
+
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
+def home():
+
+    if request.method == 'POST':
+        now = dt.datetime.now()
+        data = request.form.get('note')
+        email = request.form.get('note_email')
+        name = request.form.get('note_name')
+        date = request.form.get('note_date')
+        form = request.form.get('note_form')
+        if len(data) < 1:
+            flash('Note is too short!', category='error')
+        elif not date_checker(date):
+
+            flash('Date format is wrong!', category='error')
+        else:
+            new_note = Note(data=data, email=email, name=name, date=date, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!', category='success')
+
+            bday_d = int(date.split("/")[0])
+            bday_m = int(date.split("/")[1])
+            # print(type(bday_m), type(now.month))
+            if bday_m == now.month:
+                # print("m s")
+                if bday_d == now.day:
+                    if new_note.greet == 0:
+                        #---------------------------------------------------------
+                        # phone = "+919840025725"
+                        # client = TelegramClient("Test", 11922065, "bd5283ce86287d62813b1ec0fef8a9ec")
+                        # client.start()
+                        # destination_user_username = '919840046483'
+                        # entity = client.get_entity(destination_user_username)
+                        # client.send_message(entity=entity, message="Hi")
+                        #-----------------------------------------------------------
+                        print("sending mail...")
+                        my_email = "professorsergiomarquinasalva@gmail.com"
+                        password = "qazwsxcde12"
+
+                        connection = smtplib.SMTP("smtp.gmail.com")
+                        connection.starttls()
+                        connection.login(my_email, password)
+                        connection.sendmail(from_addr=my_email, to_addrs=email,
+                                            msg=f"Subject:Flask\n\nHappy Birthday {name}")
+                        new_note.greet = 1
+                        db.session.commit()
+
+    return render_template("home.html", user=current_user)
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
